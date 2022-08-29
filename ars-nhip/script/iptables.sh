fb28e43e031f70ddc3524dd268205603063a3a20
Daniel Mauser <dmauser@hotmail.com>
2022-08-29 11:58:18 -0500

    update

--- ars-nhip/script/iptables.sh
+++ ars-nhip/script/iptables.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+# IPtables rules:
 iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A FORWARD -i eth0 -p tcp --dport 80 -j ACCEPT
 iptables -A FORWARD -i eth0 -p tcp --dport 22 -j ACCEPT
