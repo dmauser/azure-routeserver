cfcfffbdb2f30c4b0e53da43e6462d2b0326338c
Daniel Mauser <dmauser@hotmail.com>
2022-09-20 16:57:39 -0500

    update

--- ars-nhip/script/iptables.sh
+++ ars-nhip/script/iptables.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # IPtables rules:
+iptables -F FORWARD
 iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A FORWARD -i eth0 -p tcp --dport 80 -j ACCEPT
 iptables -A FORWARD -i eth0 -p tcp --dport 443 -j ACCEPT
 iptables -A FORWARD -i eth0 -p tcp --dport 53 -j ACCEPT
 iptables -A FORWARD -i eth0 -p tcp --dport 22 -j ACCEPT
 iptables -A FORWARD -i eth0 -p tcp --dport 5201 -j ACCEPT
-iptables -A FORWARD -i eth0 -p --icmp-type echo-request -j ACCEPT
-iptables -A FORWARD -i eth0 -p --icmp-type echo-reply -j ACCEPT
+iptables -A FORWARD -i eth0 -p icmp --icmp-type echo-request -j ACCEPT
+iptables -A FORWARD -i eth0 -p icmp --icmp-type echo-reply -j ACCEPT
 iptables -A FORWARD -j DROP
 
 # Save to IPTables file for persistence on reboot
