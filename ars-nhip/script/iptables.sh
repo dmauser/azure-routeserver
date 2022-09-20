9348ed22c8114eccbf3714954c11846219ff00a5
Daniel Mauser <dmauser@hotmail.com>
2022-09-20 16:49:27 -0500

    update

--- ars-nhip/script/iptables.sh
+++ ars-nhip/script/iptables.sh
@@ -6,6 +6,8 @@ iptables -A FORWARD -i eth0 -p tcp --dport 443 -j ACCEPT
 iptables -A FORWARD -i eth0 -p tcp --dport 53 -j ACCEPT
 iptables -A FORWARD -i eth0 -p tcp --dport 22 -j ACCEPT
 iptables -A FORWARD -i eth0 -p tcp --dport 5201 -j ACCEPT
+iptables -A FORWARD -i eth0 -p --icmp-type echo-request -j ACCEPT
+iptables -A FORWARD -i eth0 -p --icmp-type echo-reply -j ACCEPT
 iptables -A FORWARD -j DROP
 
 # Save to IPTables file for persistence on reboot
