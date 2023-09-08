741b5c8b35eab44492d97080adb70c106814f505
Daniel Mauser <dmauser@hotmail.com>
2023-09-08 15:45:52 -0500

    update

--- ars-sdwan-er/script/branch-ffr.sh
+++ ars-sdwan-er/script/branch-ffr.sh
@@ -17,6 +17,7 @@ sed -i "/net.ipv6.conf.all.forwarding=1/ s/# *//" /etc/sysctl.conf
 iptables -t nat -A POSTROUTING -d 10.0.0.0/8 -j ACCEPT
 iptables -t nat -A POSTROUTING -d 172.16.0.0/12 -j ACCEPT
 iptables -t nat -A POSTROUTING -d 192.168.0.0/16 -j ACCEPT
+# iptables -t nat -A POSTROUTING -d 100.64.0.0/10 -j ACCEPT
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 
--- ars-sdwan-er/script/zerotier.sh
+++ ars-sdwan-er/script/zerotier.sh
@@ -2,3 +2,13 @@
 # Parameters
 sudo curl -s https://install.zerotier.com | sudo bash
 sudo zerotier-cli join 6ab565387ae92a6b
+
+zerotier-cli info
+zerotier-cli listnetworks
+zerotier-cli listpeers
+zerotier-cli peers
+zerotier-cli listpeers | grep -oE '([0-9a-f]{10})' | xargs -I % zerotier-cli orbit % 6ab565387ae92a6b
+zerotier-cli listmoons
+
+
+echo | echo test1 && sleep 10 && echo test2
