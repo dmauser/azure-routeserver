8118175b6719d1cb6df5d968c2c4ac6efb376f71
Daniel Mauser <dmauser@hotmail.com>
2024-09-27 17:03:06 -0500

    update

--- ars-limits/validation-routes.sh
+++ ars-limits/validation-routes.sh
@@ -7,6 +7,12 @@ az network nic show-effective-route-table -g $rg -n az-hub-lxvm-nic  -o table | 
 # now count how many routes are there in the effective route table filter by VirtualNetworkGateway
 az network nic show-effective-route-table -g $rg -n az-hub-lxvm-nic  -o table | grep "VirtualNetworkGateway" | wc -l
 
+# Loop this comand above to happen every 15 seconds
+while true; do az network nic show-effective-route-table -g $rg -n az-hub-lxvm-nic  -o table | grep "VirtualNetworkGateway" | wc -l; sleep 15; done
+
+
+
+##### File routes validation #####
 
 # on the file 7280-routes.txt count grep for network 
 cat ./ars-limits/7280-routes.txt | grep "network" | wc -l
