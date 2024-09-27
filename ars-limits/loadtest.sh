a94000edbb3660c56fe5474465d2cb6c3c716163
Daniel Mauser <dmauser@hotmail.com>
2024-09-27 18:03:01 -0500

    update

--- ars-limits/loadtest.sh
+++ ars-limits/loadtest.sh
@@ -1,16 +1,25 @@
 
 ### Deploy the lab
+# Step 1: Deploy base lab
+curl -sL https://raw.githubusercontent.com/dmauser/azure-routeserver/refs/heads/main/ars-nhip/script/1deploy.azcli | bash
+# Step 2: Configure NVA Linux VMs with FRR and peer with ARS
+curl -sL https://raw.githubusercontent.com/dmauser/azure-routeserver/refs/heads/main/ars-nhip/script/2deploy.azcli | bash
 
+### Lab diagram
+https://github.com/dmauser/azure-routeserver/blob/main/ars-nhip/media/validation2.png
+
+### Usinb bastion log on az-hub-lxnva1 
+# elevate to root
+sudo -s
 ### backup frr config
 cat /etc/frr/frr.conf > /etc/frr/frr.conf.bak
 
-### 5K routes: https://raw.githubusercontent.com/dmauser/azure-routeserver/refs/heads/main/ars-limits/5000-routes.txt
+### 5K routes: https://raw.githubusercontent.com/dmauser/azure-routeserver/refs/heads/main/ars-limits/5K-routes.txt
 rm /var/log/frr/bgpd.log #clean logs
 wget https://raw.githubusercontent.com/dmauser/azure-routeserver/refs/heads/main/ars-limits/5K-routes.txt
 cp 5K-routes.txt /etc/frr/frr.conf
 systemctl restart frr #restart FRR daemon.
 tail -f /var/log/frr/bgpd.log
-
 
 ### 10K routes: https://raw.githubusercontent.com/dmauser/azure-routeserver/refs/heads/main/ars-limits/10K-routes.txt
 rm /var/log/frr/bgpd.log #clean logs
--- ars-limits/validation-routes.sh
+++ ars-limits/validation-routes.sh
@@ -1,18 +1,12 @@
+# Optional: set the subscription
 az account set -s VSE-Sub
+# Resource group
 rg=lab-ars-nhip
-
-# get az-hub-lxvm-nic effective routes and grep Virtual network gateway -o table
-az network nic show-effective-route-table -g $rg -n az-hub-lxvm-nic  -o table | grep "VirtualNetworkGateway"
-
-# now count how many routes are there in the effective route table filter by VirtualNetworkGateway
-az network nic show-effective-route-table -g $rg -n az-hub-lxvm-nic  -o table | grep "VirtualNetworkGateway" | wc -l
 
 # Loop this comand above to happen every 15 seconds
 while true; do az network nic show-effective-route-table -g $rg -n az-hub-lxvm-nic  -o table | grep "VirtualNetworkGateway" | wc -l; sleep 15; done
 
-
-
-##### File routes validation #####
+##### Route files validation #####
 
 # on the file 7280-routes.txt count grep for network 
 cat ./ars-limits/7280-routes.txt | grep "network" | wc -l
