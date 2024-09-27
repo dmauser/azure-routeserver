b3eb77e6eb4f5fabdb4a2e036352a82142ae9c6b
Daniel Mauser <dmauser@hotmail.com>
2024-09-27 17:06:22 -0500

    Update loadtest.sh

--- ars-limits/loadtest.sh
+++ ars-limits/loadtest.sh
@@ -33,12 +33,10 @@ cp 8K-routes.txt /etc/frr/frr.conf
 systemctl restart frr #restart FRR daemon.
 tail -f /var/log/frr/bgpd.log
 
-
-
-### 7280 routes: https://raw.githubusercontent.com/dmauser/azure-routeserver/refs/heads/main/ars-limits/7280-routes.txt
+### 6473 routes: https://raw.githubusercontent.com/dmauser/azure-routeserver/refs/heads/main/ars-limits/6473-routes.txt
 rm /var/log/frr/bgpd.log #clean logs
-wget https://raw.githubusercontent.com/dmauser/azure-routeserver/refs/heads/main/ars-limits/7280-routes.txt
-cp 7280-routes.txt /etc/frr/frr.conf
+wget https://raw.githubusercontent.com/dmauser/azure-routeserver/refs/heads/main/ars-limits/6473-routes.txt
+cp 6473-routes.txt /etc/frr/frr.conf
 systemctl restart frr #restart FRR daemon.
 tail -f /var/log/frr/bgpd.log
 
