8afd103d5d643b148722ab377d1a098a7cec2fb8
Daniel Mauser <dmauser@hotmail.com>
2024-09-27 09:44:26 -0500

    added log

--- ars-limits/loadtest.sh
+++ ars-limits/loadtest.sh
@@ -1,6 +1,6 @@
-### 5000 routes: https://raw.githubusercontent.com/dmauser/AzureVM-Router/master/conf/5000-bgproutes.txt
+### 5000 routes: https://raw.githubusercontent.com/dmauser/azure-routeserver/refs/heads/main/ars-limits/5000-routes.txt
 rm /var/log/frr/bgpd.log #clean logs
-wget https://raw.githubusercontent.com/dmauser/AzureVM-Router/master/conf/5000-bgproutes.txt
+wget https://raw.githubusercontent.com/dmauser/azure-routeserver/refs/heads/main/ars-limits/5000-routes.txt
 cp 5000-bgproutes.txt /etc/frr/bgpd.conf
 systemctl restart frr.service #restart FRR daemon.
 tail -f /var/log/frr/bgpd.log
