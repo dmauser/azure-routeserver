2b5f378273afd3fccfa5b051aac3ab1551d94b28
Daniel Mauser <dmauser@hotmail.com>
2024-09-27 16:44:48 -0500

    update

--- ars-limits/validation-routes.sh
+++ ars-limits/validation-routes.sh
@@ -6,3 +6,10 @@ az network nic show-effective-route-table -g $rg -n az-hub-lxvm-nic  -o table | 
 
 # now count how many routes are there in the effective route table filter by VirtualNetworkGateway
 az network nic show-effective-route-table -g $rg -n az-hub-lxvm-nic  -o table | grep "VirtualNetworkGateway" | wc -l
+
+
+# on the file 7280-routes.txt count grep for network 
+cat ./ars-limits/7280-routes.txt | grep "network" | wc -l
+
+# on the file 5000-routes.txt count grep for network
+cat ./ars-limits/5000-routes.txt | grep "network" | wc -l
